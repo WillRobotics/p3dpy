03641119619079ee1fec608556321882669b9e34
nekanat <nekanat.stock@gmail.com>
2021-03-27 13:07:55 +0900

    segmentation example

--- examples/segmentation_example.py
+++ examples/segmentation_example.py
@@ -8,10 +8,13 @@ pc = pp.io.load_pcd('data/test0.pcd')
 pc = pp.filter.remove_invalid_points(pc)
 pc = pp.filter.voxel_grid_filter(pc, 0.01)
 print(pc.points)
-plane, mask = pp.segmentation.segmentation_plane(pc)
+plane, mask = pp.segmentation.segmentation_plane(pc, dist_thresh=0.01)
 print(plane, sum(mask))
 
 fig = plt.figure(figsize = (8, 8))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(pc.points[:, 0], pc.points[:, 1], pc.points[:, 2], s = 40, c = "blue")
+plane_pts = pc.points[mask, :]
+not_plane_pts = pc.points[~mask, :]
+ax.scatter(plane_pts[:, 0], plane_pts[:, 1], plane_pts[:, 2], s=20, c="blue")
+ax.scatter(not_plane_pts[:, 0], not_plane_pts[:, 1], not_plane_pts[:, 2], s=20, c="red")
 plt.show()
