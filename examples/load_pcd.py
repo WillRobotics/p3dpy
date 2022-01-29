c9be7f0c2d277e7b9e8018bc40cae03982d57497
nekanat <nekanat.stock@gmail.com>
2022-01-29 14:29:58 +0900

    lzf example

--- examples/load_pcd.py
+++ examples/load_pcd.py
@@ -3,7 +3,8 @@ import p3dpy as pp
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-pc = pp.io.load_pcd('data/bunny.pcd')
+pc = pp.io.load_pcd('data/bunny_lzf.pcd')
+pc = pp.filter.remove_invalid_points(pc)
 pc = pp.filter.voxel_grid_filter(pc, 0.02)
 pc.compute_normals(0.1)
 print(pc.data)
