cba07ec08208fb55dd790f0833772fbb5c8b076a
neka-nat <nekanat.stock@gmail.com>
2025-12-24 19:26:50 +0900

    v0.17.0

--- p3dpy/filter.py
+++ p3dpy/filter.py
@@ -8,21 +8,81 @@ from . import pointcloud
 
 
 def remove_invalid_points(pc: pointcloud.PointCloud) -> pointcloud.PointCloud:
+    """Remove invalid points from the point cloud.
+
+    Parameters
+    ----------
+    pc: pointcloud.PointCloud
+        The point cloud to remove invalid points from.
+
+    Returns
+    -------
+    pointcloud.PointCloud
+        The point cloud with invalid points removed.
+    """
     return pointcloud.PointCloud(pc.finalize().data[np.isfinite(pc.data).any(axis=1)], field=pc.field)
 
 
 def random_sampling(pc: pointcloud.PointCloud, n_sample: int) -> pointcloud.PointCloud:
+    """Randomly sample points from the point cloud.
+
+    Parameters
+    ----------
+    pc: pointcloud.PointCloud
+        The point cloud to sample points from.
+    n_sample: int
+        The number of points to sample.
+
+    Returns
+    -------
+    pointcloud.PointCloud
+        The point cloud with sampled points.
+    """
     return pointcloud.PointCloud(pc.finalize().data[np.random.randint(len(pc), size=n_sample), :], field=pc.field)
 
 
 def pass_through_filter(
     pc: pointcloud.PointCloud, min_lim: float, max_lim: float, axis: int = 0
 ) -> pointcloud.PointCloud:
+    """Pass through filter
+
+    Parameters
+    ----------
+    pc: pointcloud.PointCloud
+        The point cloud to pass through.
+    min_lim: float
+        The minimum limit.
+    max_lim: float
+        The maximum limit.
+    axis: int
+        The axis to pass through.
+
+    Returns
+    -------
+    pointcloud.PointCloud
+        The point cloud with passed through points.
+    """
     axis_pc = pc.points[:, axis]
     return pointcloud.PointCloud(pc.data[(axis_pc >= min_lim) & (axis_pc <= max_lim), :], field=pc.field)
 
 
 def radius_outlier_removal(pc: pointcloud.PointCloud, radius: float, neighbor_counts: int) -> pointcloud.PointCloud:
+    """Radius outlier removal
+
+    Parameters
+    ----------
+    pc: pointcloud.PointCloud
+        The point cloud to remove outliers from.
+    radius: float
+        The radius of the sphere.
+    neighbor_counts: int
+        The number of neighbors to consider.
+
+    Returns
+    -------
+    pointcloud.PointCloud
+        The point cloud with outliers removed.
+    """
     tree = cKDTree(pc.points)
     mask = [len(tree.query_ball_point(p, radius)) > neighbor_counts for p in pc.points]
     return pointcloud.PointCloud(pc.data[mask, :], pc.field)
@@ -31,6 +91,22 @@ def radius_outlier_removal(pc: pointcloud.PointCloud, radius: float, neighbor_co
 def statistical_outlier_removal(
     pc: pointcloud.PointCloud, k_neighbors: int, std_ratio: float
 ) -> pointcloud.PointCloud:
+    """Statistical outlier removal
+
+    Parameters
+    ----------
+    pc: pointcloud.PointCloud
+        The point cloud to remove outliers from.
+    k_neighbors: int
+        The number of neighbors to consider.
+    std_ratio: float
+        The standard deviation ratio.
+
+    Returns
+    -------
+    pointcloud.PointCloud
+        The point cloud with outliers removed.
+    """
     tree = cKDTree(pc.points)
     dd, _ = tree.query(pc.points, k_neighbors)
     avg_d = dd.mean(axis=1)
@@ -45,14 +121,14 @@ def voxel_grid_filter(pc: pointcloud.PointCloud, voxel_size: float) -> pointclou
     Parameters
     ----------
     pc: pointcloud.PointCloud
-        Input point cloud.
+        The point cloud to filter.
     voxel_size: float
-        The length of one side of the voxel.
+        The size of the voxel.
 
     Returns
     -------
     pointcloud.PointCloud
-        Voxel-averaged point cloud.
+        The point cloud with filtered points.
     """
 
     class SumPoints:
